96fc2c2fe284bfbe2f1e83e6d659ac26c3401964
Jeremy Her <91761028+herj043@users.noreply.github.com>
2021-11-03 22:17:45 +0000

    Added comments

--- main.py
+++ main.py
@@ -21,13 +21,18 @@ for breed in all_breeds:
 # but NOT bulldogs
 # Examples include: poodles, retrievers, terriers, mastiffs, spaniels, setters, & more
 
-# Display all bulldog subbreeds
-print("for examples try bulldog, poodle, retriever, terrier, mastiff, spaniel, and settler, for more try others listed above")
+#Display examples of breeds available for user input
+print("for examples try bulldog, poodle, retriever, terrier, mastiff, spaniel, and setter, for more try others listed above")
+
+#repeats code within the loop and counts the loop
 for i in range(1,4,1):
   print(i)
+
+  #Uses the user input to insert a subreed
   requested_subbreeds = breeds[input("\nEnter a breed to find a available subreeds(no caps) = ")]
 
-
+  #Uses the user input and picks out a breed related to that input, then inserts the 
+  #avaiable subreeds.
   for sub in requested_subbreeds:
     print(sub)
 
